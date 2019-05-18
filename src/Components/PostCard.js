4c347ad6f2f37aa2c11b03095ba6d6c98a177dc0
seungsukim <seungsu0407@gmail.com>
2019-05-18 19:24:25 +0900

    #8.4 PostCard Component

---
+++ src/Components/PostCard.js
@@ -0,0 +1,70 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { HeartFull, CommentFull } from "./Icons";
+
+const Overlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.6);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0;
+  transition: opacity 0.2s linear;
+  svg {
+    fill: white;
+  }
+`;
+
+const Container = styled.div`
+  ${props => props.theme.whiteBox}
+  background-image: url(${props => props.bg});
+  background-size: cover;
+  background-position: center;
+  cursor: pointer;
+  &:hover {
+    ${Overlay} {
+      opacity: 1;
+    }
+  }
+`;
+
+const Number = styled.div`
+  color: white;
+  display: flex;
+  align-items: center;
+  &:first-child {
+    margin-right: 20px;
+  }
+`;
+
+const NumberText = styled.span`
+  margin-left: 10px;
+  font-size: 17px;
+`;
+
+const PostCard = ({ likeCount, commentCount, file }) => (
+  <Container bg={file.url}>
+    <Overlay>
+      <Number>
+        <HeartFull />
+        <NumberText>{likeCount}</NumberText>
+      </Number>
+      <Number>
+        <CommentFull />
+        <NumberText>{commentCount}</NumberText>
+      </Number>
+    </Overlay>
+  </Container>
+);
+
+PostCard.propTypes = {
+  likeCount: PropTypes.number.isRequired,
+  commentCount: PropTypes.number.isRequired,
+  file: PropTypes.shape({
+    url: PropTypes.string.isRequired
+  }).isRequired
+};
+
+export default PostCard;
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -4,17 +4,34 @@ import PropTypes from "prop-types";
 import FatText from "../../Components/FatText";
 import Loader from "../../Components/Loader";
 import UserCard from "../../Components/UserCard";
+import PostCard from "../../Components/PostCard";
 
 const Wrapper = styled.div`
-  height: 50vh;
+  height: minmax(50vh, auto);
 `;
 
 const Section = styled.div`
   display: grid;
   grid-gap: 25px;
-  grid-template-columns: repeat(4, 1fr);
-  grid-auto-rows: 160px;
+  grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+  grid-auto-rows: 1fr;
   margin-bottom: 50px;
+`;
+
+const PostSection = styled(Section)`
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-auto-rows: 1fr;
+  ::before {
+    content: "";
+    width: 0;
+    padding-bottom: 100%;
+    grid-row: 1 / 1;
+    grid-column: 1 / 1;
+  }
+  > *:first-child {
+    grid-row: 1 / 1;
+    grid-column: 1 / 1;
+  }
 `;
 
 const SearchPresenter = ({ term, loading, data }) => {
@@ -51,13 +68,20 @@ const SearchPresenter = ({ term, loading, data }) => {
             ))
           )}
         </Section>
-        <Section>
+        <PostSection>
           {data.searchPost.length === 0 ? (
             <FatText text="No posts found." />
           ) : (
-            data.searchPost.map(post => null)
+            data.searchPost.map(post => (
+              <PostCard
+                key={post.id}
+                likeCount={post.likeCount}
+                commentCount={post.commentCount}
+                file={post.files[0]}
+              />
+            ))
           )}
-        </Section>
+        </PostSection>
       </Wrapper>
     );
   }
@@ -69,7 +93,9 @@ SearchPresenter.propTypes = {
   data: PropTypes.shape({
     searchPost: PropTypes.arrayOf(
       PropTypes.shape({
+        id: PropTypes.string.isRequired,
         likeCount: PropTypes.number.isRequired,
+        commentCount: PropTypes.number.isRequired,
         files: PropTypes.arrayOf(
           PropTypes.shape({ url: PropTypes.string.isRequired })
         )
